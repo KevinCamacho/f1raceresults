bf469d6a31fe3714e8d5ae39740836ba9a85ccfd
Kevin Camacho <kpcamacho@hotmail.com>
2024-03-26 19:49:28 -0400

    added type file for driver

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,6 @@
 import { IMeeting } from "./IMeeting";
 import { ISession } from "./ISession";
 import { IPosition } from "./IPosition";
+import { IDriver } from "./IDriver";
 
-export type { IMeeting, ISession, IPosition };
+export type { IMeeting, ISession, IPosition, IDriver };
