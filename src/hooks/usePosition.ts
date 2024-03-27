c699e69a30d2f90c7fe613a3ddb17e325d07406a
Kevin Camacho <kpcamacho@hotmail.com>
2024-03-27 13:04:43 -0400

    getFinishingPositions now correctly returns the race finishing positions, in order

--- src/hooks/usePosition.ts
+++ src/hooks/usePosition.ts
@@ -25,10 +25,24 @@ const getFinishingPositions = (sessionKey: number): Promise<IPosition[]> => {
           };
         }),
       )
-      //this sorts the finishing positions in descending order
+      //this sorts the finishing positions in descending order by timestamp
       .then((data: IPosition[]) =>
         data.sort((a, b) => b.parsed_date - a.parsed_date),
       )
+      //dedupes by position, taking the first of each P1-P20 we see
+      .then((data: IPosition[]) => {
+        const positionsSeen: number[] = [];
+        return data.filter((position: IPosition) => {
+          if (positionsSeen.includes(position.position)) {
+            return false;
+          } else {
+            positionsSeen.push(position.position);
+            return true;
+          }
+        });
+      })
+      //this sorts the deduped finishing positions in P1-P20 order
+      .then((data: IPosition[]) => data.sort((a, b) => a.position - b.position))
   );
 };
 
