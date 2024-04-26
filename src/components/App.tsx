06e18289ca88383b9b5ae66de48d9c2d977193ab
Kevin Camacho <kpcamacho@hotmail.com>
2024-04-25 21:38:31 -0400

    dropdown in nav bar that lets you select year

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,57 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import "./App.scss";
 import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
 import SeasonRaceWinnerList from "./Race/SeasonRaceWinnerList";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 const App: FC = () => {
+  const years: string[] = ["2020", "2021", "2022", "2023", "2024"];
+  const [selectedYear, setSelectedYear] = useState<string>(
+    years[years.length - 1],
+  );
+
+  const onNavItemClick = (eventKey: any, event: Object) => {
+    setSelectedYear(eventKey);
+  };
+
   return (
-    <Container fluid="md">
-      <SeasonRaceWinnerList year="2024" />
-    </Container>
+    <>
+      <Navbar expand="sm" className="bg-body-tertiary">
+        <Container fluid>
+          <Navbar.Brand>F1 Race Results</Navbar.Brand>
+
+          <Navbar.Collapse
+            id="navbar-dark-example"
+            className="d-flex justify-content-end"
+          >
+            <Nav>
+              <NavDropdown
+                id="seasonSelectionDropdown"
+                title="Season"
+                menuVariant="dark"
+                onSelect={onNavItemClick}
+              >
+                {years.map((year: string, index: number) => (
+                  <NavDropdown.Item
+                    as="button"
+                    key={`dropdownYear${index}`}
+                    eventKey={year}
+                    active={selectedYear === year}
+                  >
+                    {year}
+                  </NavDropdown.Item>
+                ))}
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Container fluid="md" className="mt-3">
+        <SeasonRaceWinnerList year={selectedYear} />
+      </Container>
+    </>
   );
 };
 
