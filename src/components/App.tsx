0f4c3376e4d400eed27fdc89b5095bf6fa0d1d02
Kevin Camacho <kpcamacho@hotmail.com>
2025-04-11 20:57:36 -0400

    moving off of CRA

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,7 +7,7 @@ import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
 
 const App: FC = () => {
-  const years: string[] = ["2023", "2024"];
+  const years: string[] = ["2023", "2024", "2025"];
   const [selectedYear, setSelectedYear] = useState<string>(
     years[years.length - 1],
   );
